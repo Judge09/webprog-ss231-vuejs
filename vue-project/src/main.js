049792a53f99ebd11049dbdeb1bdaac31c33fd7b
Judge09 <133473201+Judge09@users.noreply.github.com>
2025-02-12 10:58:55 +0000

    cooment form

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import personalwebsite from './components/personalwebsite.vue'
- 
+import Country from './components/Country.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
+
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
-app.component('personalwebsite', personalwebsite)
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
+
+
